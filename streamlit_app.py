7917d3a076ec20e2bb54c58499f9938fbd9ca2b7
Milot Hyseni <milothysni@hotmail.com>
2026-06-11 16:50:06 +0200

    project setup

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="ICSHPS Demo", layout="wide")
+
+st.title("ICSHPS - Intelligent Candidate Screening & Hiring Pipeline System")
+st.write("demo placeholder.")
+
+st.info("placeholder")
